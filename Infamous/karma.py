f2ccbea749547281ec771c3441ab539eb109d2a7
NEXONHOST <amits62773@gmail.com>
2025-10-26 18:06:52 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -1,66 +1,64 @@
-# <============================================== IMPORTS =========================================================>
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton as ib
 from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
 
-# <============================================== CONSTANTS =========================================================>
+# ============================================== CONSTANTS ==============================================
 START_IMG = [
     "https://files.catbox.moe/1jvj34.jpg",
     "https://files.catbox.moe/idjyr8.jpg",
 ]
 
+HEY_IMG = "https://files.catbox.moe/0lbgjt.jpg"
+
+ALIVE_ANIMATION = [
+    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
+    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
+    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
+    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
+]
+
+FIRST_PART_TEXT = "✨ ʜᴇʟʟᴏ {} . . ."
+
 PM_START_TEXT = (
-    "✨ *ɪ ᴀᴍ 𝚁𝚘𝚜𝚜𝚢, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ "
+    "✨ ɪ ᴀᴍ 𝚁𝚘𝚜𝚜𝚢, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ "
     "ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ "
-    "ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
+    "ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ"
 )
 
 HELP_STRINGS = """
-🫧 *ROSSY* 🫧 [ㅤ](https://files.catbox.moe/xxiylg.jpg)
+🫧 ROSSY 🫧 ㅤ
 
-☉ *Here, you will find a list of all the available commands.*
+☉ Here, you will find a list of all the available commands.
 
 ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /
 """
 
-# <============================================== BUTTONS =========================================================>
+# ============================================== BUTTONS ==============================================
+# Private chat start buttons (vertical + clean)
 START_BTN = [
-    [InlineKeyboardButton("⇦ ADD ME ⇨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-    [InlineKeyboardButton("HELP", callback_data="extra_command_handler")],
-    [InlineKeyboardButton("DETAILS", callback_data="Miko_")],
-    [InlineKeyboardButton("CREATOR", url=f"tg://user?id={OWNER_ID}")],
+    [ib("⇦ ADD ME ⇨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+    [ib("HELP", callback_data="extra_command_handler")],
+    [ib("DETAILS", callback_data="Miko_")],
+    [ib("CREATOR", url=f"tg://user?id={OWNER_ID}")],
 ]
 
+# Group chat start buttons (horizontal + neat)
 GROUP_START_BTN = [
-    [InlineKeyboardButton("⇦ ADD ME ⇨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+    [ib("⇦ ADD ME ⇨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
     [
-        InlineKeyboardButton("SUPPORT", url=f"https://t.me/{SUPPORT_CHAT}"),
-        InlineKeyboardButton("CREATOR", url=f"tg://user?id={OWNER_ID}")
+        ib("SUPPORT", url=f"https://t.me/{SUPPORT_CHAT}"),
+        ib("CREATOR", url=f"tg://user?id={OWNER_ID}")
     ],
 ]
 
+# Alive / status buttons (two rows)
 ALIVE_BTN = [
     [
-        InlineKeyboardButton("UPDATES", url="https://t.me/Hydra_Updates"),
-        InlineKeyboardButton("SUPPORT", url="https://t.me/hydraXsupport"),
+        ib("UPDATES", url="https://t.me/Hydra_Updates"),
+        ib("SUPPORT", url="https://t.me/hydraXsupport"),
     ],
-    [InlineKeyboardButton("⇦ ADD ME ⇨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+    [ib("⇦ ADD ME ⇨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
 ]
-
-# <============================================== /start HANDLER =========================================================>
-@Client.on_message(filters.command("start"))
-async def start(client, message):
-    # Check chat type
-    if message.chat.type == "private":
-        markup = InlineKeyboardMarkup(START_BTN)
-        text = PM_START_TEXT.format(message.from_user.first_name)
-    else:
-        markup = InlineKeyboardMarkup(GROUP_START_BTN)
-        text = f"✨ Hello {message.from_user.first_name}! I am Rossy 🤖"
-
-    # Send start message with buttons
-    await message.reply_text(
-        text=text,
-        reply_markup=markup,
-        disable_web_page_preview=True
-    )
